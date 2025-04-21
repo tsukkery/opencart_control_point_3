03ea2e4d769561712748c94cc5918281ef6f407a
tsukkery <dariya_first@mail.ru>
2025-04-21 13:14:20 +0300

    Added allure

--- src/test/java/BasePage.py
+++
@@ -1,7 +0,0 @@
-class BasePage:
-    BASE_URL = "http://localhost:8081/"
-
-    def __init__(self, driver):
-        self.driver = driver
-        self.driver.implicitly_wait(3)
-        self.driver.get(BasePage.BASE_URL)
